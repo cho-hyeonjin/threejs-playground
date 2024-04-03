7e52e680b7a87dbd11a84448d9bf8618620f3f67
cho-hyeonjin <gusls2005@gmail.com>
2024-04-03 22:28:41 +0900

    📦Material

--- 1-cube/src/main.js
+++ 1-cube/src/main.js
@@ -10,18 +10,15 @@ function init() {
     // alpha: true, // 배경 검정색 아니고 투명하게 하려면 true로
     antialias: true, // 이미지 모서리마다 노이즈 또는 계단처럼 느껴지는 질감 없어지게 처리하는 속성
   });
-
-  // 🖥️📐 렌더러 사이즈 세팅
+  // > 🖥️📐 렌더러 사이즈 세팅
   renderer.setSize(window.innerWidth, window.innerHeight); // canvas를 화면에 꽉 차는 사이즈로 설정
-
-  // canvas를 body의 자식요소로 추가 (renderer.domElement == canvas)
+  // > 🖥️ document.body 노드의 자식요소로 렌더러의 DOM요소(canvas) 추가 (renderer.domElement == canvas)
   document.body.appendChild(renderer.domElement);
 
   // 🏞️ Scene 생성 (3js의 내장 클래스 Scene으로 찍어낸 3D 컨텐츠를 담는 인스턴스)
   const scene = new THREE.Scene();
 
-  // camera 추가 - 종류 아주 많음. 여기서는 물체에 대한 원근감을 표현할 수 있는 카메라 'perspective' 카메라 추가
-
+  // 📹 Camera 생성
   const camera = new THREE.PerspectiveCamera(
     75, // 시야각(fov, field of view)
     window.innerWidth / window.innerHeight, // 카메라 종횡비
@@ -34,7 +31,20 @@ function init() {
   const geometry = new THREE.BoxGeometry(2, 2, 2);
   // > 3js에서 제공하는 내장 Mesh 클래스로 3D 오브젝트의 질감 (Material) 생성
   // const material = new THREE.MeshBasicMaterial({ color: 0xcc99ff }); // lights의 영향을 받지 않는 geometry라 light로 음영표현이 안됨
-  const material = new THREE.MeshStandardMaterial({ color: 0xcc99ff });
+  const material = new THREE.MeshStandardMaterial({
+    // color: 0xffa500 // 16진수 넘버타입 hex 코드
+    // color: '#ffa500' // 16진수 문자열타입 hex 코드
+    // color: 'orange' // 컬러명
+    color: new THREE.Color(0xffa500), // 3js에 내장된 Color 클래스 인스턴스
+    // transparent: true, // 투명도 설정
+    // opacity: 0.5, // 투명도 정도 설정
+    // visible: false, // 안 보이게
+    // wireframe: true, // Material 골격 확인
+    // side: THREE.BackSide, // 보여지는 기준면
+  });
+
+  material.color = new THREE.Color(0x00c896); // material 생성 후에 컬러 부여하는 방식도 가능
+
   // > Mesh 오브젝트 생성✨ (Mesh클래스에 Geometry, Material 전달해서 찍어낸 인스턴스)
   const cube = new THREE.Mesh(geometry, material);
   // Mesh 오브젝트 Scene에 추가
