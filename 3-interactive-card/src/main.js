2ea40338e1cdfd930f2ca8e62d7e55ee02471284
cho-hyeonjin <gusls2005@gmail.com>
2024-04-13 11:21:32 +0900

    Implementing metallic texture using 🔦DirectionalLight's roughness and metalness properties✨

--- 3-interactive-card/src/main.js
+++ 3-interactive-card/src/main.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GUI } from "lil-gui";
 import Card from "./Card";
 
 window.addEventListener("load", function () {
@@ -7,6 +8,8 @@ window.addEventListener("load", function () {
 });
 
 function init() {
+  const gui = new GUI();
+
   const renderer = new THREE.WebGLRenderer({
     antialias: true,
     alpha: true, // renderer 배경 투명하게
@@ -32,16 +35,41 @@ function init() {
   const card = new Card({
     width: 10,
     height: 15.8,
-    color: "#fda4ba",
+    color: "#0077ff",
   });
 
   scene.add(card.mesh);
 
-  const ambientLight = new THREE.AmbientLight(0xffffff, 4);
+  const cardFolder = gui.addFolder("Card");
+
+  cardFolder
+    .add(card.mesh.material, "roughness")
+    .min(0)
+    .max(1)
+    .step(0.01)
+    .name("material.roughness");
+
+  cardFolder
+    .add(card.mesh.material, "metalness")
+    .min(0)
+    .max(1)
+    .step(0.01)
+    .name("material.metalness");
+
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
 
   ambientLight.position.set(-5, -5, -5);
 
   scene.add(ambientLight);
+
+  // 카드 메탈 질감 표현 위한 directionalLight
+  const directionalLight1 = new THREE.DirectionalLight(0xffffff, 0.6);
+  const directionalLight2 = directionalLight1.clone();
+
+  directionalLight1.position.set(1, 1, 3);
+  directionalLight2.position.set(-1, 1, -3);
+
+  scene.add(directionalLight1, directionalLight2);
 
   render();
 
