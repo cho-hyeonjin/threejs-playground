bca01607b1237462d1e31d7352e4277e9b40e93a
cho-hyeonjin <gusls2005@gmail.com>
2024-04-15 17:30:36 +0900

    add 3D GLTF  🧒🏼Cute tough girl Model✨

--- 7-character-controller/src/main.js
+++ 7-character-controller/src/main.js
@@ -1,10 +1,11 @@
 import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
 window.addEventListener("load", function () {
   init();
 });
 
-function init() {
+async function init() {
   const renderer = new THREE.WebGLRenderer({
     antialias: true,
   });
@@ -22,7 +23,25 @@ function init() {
     500
   );
 
-  camera.position.z = 5;
+  camera.position.set(0, 5, 20);
+
+  const gltfLoader = new GLTFLoader();
+
+  const gltf = await gltfLoader.loadAsync("models/character.gltf");
+
+  const model = gltf.scene;
+
+  model.scale.set(0.1, 0.1, 0.1);
+
+  scene.add(model);
+
+  camera.lookAt(model.position);
+
+  const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x333333);
+
+  hemisphereLight.position.set(0, 20, 10);
+
+  scene.add(hemisphereLight);
 
   render();
 
